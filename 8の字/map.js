4a9114d3005114254a1b69674bdc79318b8588bb
22S25 <137862001+22S25@users.noreply.github.com>
2023-09-26 13:45:41 +0900

    9/15

---
+++ 8の字/map.js
@@ -0,0 +1,129 @@
+var Map = function() {
+};
+Map.prototype = {
+	"map": [
+		[1,1,1,1,1,1,1,1,1,1,1,1],
+		[1,1,1,1,1,1,1,1,1,1,1,1],
+		[1,1,1,1,1,1,1,1,1,1,1,1],
+		[1,1,1,5,0,0,0,1,5,1,1,1],
+		[1,1,1,0,1,2,0,1,0,1,1,1],
+		[1,1,1,0,1,0,0,1,0,1,1,1],
+		[1,1,1,0,1,0,0,1,0,1,1,1],
+		[1,1,1,0,1,0,0,0,0,1,1,1],
+		[1,1,1,5,0,5,1,1,1,1,1,1],
+		[1,1,1,1,1,1,1,1,1,1,1,1],
+		[1,1,1,1,1,1,1,1,1,1,1,1],
+		[1,1,1,1,1,1,1,1,1,1,1,1]
+	],
+	"start": {
+		"x": 5,
+		"y": 4,
+		"direction": 2,
+		"life": 65534,
+	},
+	"hint": "白いマスをすべて通ってゴールしよう‼",
+	"state": 0,
+	"goals": 4,
+	"patterns": 1,
+	"blocksLimit": 0,
+	"links": {
+		"question": "8の字",
+		"previous": "q3-3",
+		"next": ""
+	},
+	"robot": {
+		"type": 2,
+		"Basic": {
+			"forward": true,
+			"turn_right": true,
+			"turn_left": true,
+			"nop": true
+		},
+		"Standard": {
+			"floor_color_is": false,
+			"robot_direction_is": false,
+			"movable_is": false
+		},
+		"Advanced": {
+			"times_loop": true,
+			"floor_color_loop": true,
+			"movable_loop": true
+		},
+		"Expert": {
+			"write_register": true,
+			"read_register": true,
+			"get_floor_color": true,
+			"get_direction": true
+		},
+		"Enhanced": {
+			"values_equal_is": true,
+			"values_equal_loop": true,
+			"infinity_loop": true,
+			"is_movable_to": true
+		},
+		"Superior": {
+			"add_register": true,
+			"sub_register": true,
+			"add_register_index": true,
+			"sub_register_index": true,
+			"set_register_index": true,
+			"get_register_index": true
+		},
+		"Replete": {
+			"read_cell_value": true,
+			"read_cell_value_index": true,
+			"write_cell_value": true,
+			"values_compare": true,
+			"expression_if": true,
+			"expression_loop": true
+		},
+		"Master": {
+		}
+	},
+	"chars": [
+		[ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ],
+		[ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ],
+		[ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ],
+		[ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ],
+		[ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ],
+		[ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ],
+		[ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ],
+		[ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ],
+		[ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ],
+		[ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ],
+		[ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ],
+		[ -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 ]
+	],
+	"hintBlocks": '<xml xmlns="https://developers.google.com/blockly/xml"><block type="times_loop" x="10" y="10"><statement name="equals"><block type="forward"></block></statement></block></xml>',
+	"map2": [],
+	"chars2": [],
+	
+	"image_file_dir": '../img/'
+};
+
+/**
+ * コード実行前の処理
+ */
+Map.prototype.beforeStart = function(pattern) {
+};
+/**
+ * ターンごとに発生する処理
+ */
+Map.prototype.afterMoved = function(t, pos) {
+	if (Map.prototype.map[pos.y][pos.x] == 0 || Map.prototype.map[pos.y][pos.x] == 5) {
+		switch(pos.direction) {
+			case 0:
+				Map.prototype.map[pos.y + 1][pos.x] = 6;
+				break;
+			case 1:
+				Map.prototype.map[pos.y][pos.x - 1] = 6;
+				break;
+			case 2:
+				Map.prototype.map[pos.y - 1][pos.x] = 6;
+				break;
+			case 3:
+				Map.prototype.map[pos.y][pos.x + 1] = 6;
+				break;
+		}
+	}
+};
